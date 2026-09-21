9ad27ee5c73767912d87be07fa7e6df33b021f01
kyosu-1 <sho013039@gmail.com>
2026-09-22 02:12:55 +0900

    Revert "matcher: マッチング間隔 0.5s → 0.1s"
    This reverts commit aa8701851850db459a2a969e6183420442b7b046.

--- etc/isu3/home/env.sh
+++ etc/isu3/home/env.sh
@@ -6,5 +6,4 @@ ISUCON_DB_PASSWORD="isucon"
 ISUCON_DB_NAME="isuride"
 
 # マッチング間隔（秒）
-# マッチング間隔（秒）。椅子が足りない状況では、解放された椅子が次のマッチングまで待つ時間がそのまま無駄になる
-ISUCON_MATCHING_INTERVAL=0.1
+ISUCON_MATCHING_INTERVAL=0.5
